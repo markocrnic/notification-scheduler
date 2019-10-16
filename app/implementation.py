0b0e6f640cf6e4c4d9eb9ccca2e46ce645a60edf
markocrnic <markons996@gmail.com>
2019-10-16 11:07:07 +0200

    First stage of code refactoring done.

--- app/implementation.py
+++ app/implementation.py
@@ -1,42 +1,8 @@
 import json
-import ftputil
-from ast import literal_eval
 from flask import jsonify
 import requests
 from datetime import date, datetime
-import time
-
-
-def readpathsfromftp():
-    try:
-        a_host = ftputil.FTPHost('10.0.200.68', 'empiry', '3mp1ry')
-
-        for (dirname, subdirs, files) in a_host.walk("/projects/planthealthcare/api-gateway/"):
-            for f in files:
-                if f == 'paths.txt':
-                    a_host.download(dirname + f, f)
-                    with open(f) as txtfile:
-                        content = txtfile.read()
-                        print(str(content))
-        a_host.close()
-
-        return literal_eval(content)
-
-    except Exception as e:
-        print(e)
-        a_host.close()
-
-
-def getpath(path):
-    path_dict = readpathsfromftp()
-    apiname = path.split('/')[0]
-    if apiname in path_dict:
-        apipath = path_dict[apiname] + path
-        print(apipath)
-        return apipath
-    else:
-        print('Path not found: ' + path)
-        return '404'
+from api_management import getpath
 
 
 user2flower_get_request = getpath('users2flowers/')
